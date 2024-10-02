85f291deec560fba034ee2668d4989351f3a6549
NjorogeMaurice <njoroge0330@gmail.com>
2024-10-02 13:00:25 +0300

    Initial Commit

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ async def generate_response_with_rag(request: QueryRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5583)
